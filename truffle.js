6bf59e18cede61852758524f0b672657f8326948
JonasOli <jonasfissicaro13@gmail.com>
2018-11-25 13:59:52 -0200

    contrato deployed

--- truffle.js
+++ truffle.js
@@ -1,11 +1,28 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infuraAPIKey = "a6bc10219ba2409a8f7dc68a498b7a64"
+var mnemonic = "bench ridge icon space engage gather ride cash human travel expire budget"
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+  solc: {
+    optimazer: {
+      enable: true,
+      runs: 200
+    }
+  },
   networks: {
     development: {
-      host: "10.201.12.111",
+      host: "192.168.0.12",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infuraAPIKey)
+      },
+      network_id: 3
     }
   }
 };
